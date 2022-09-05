4912dfeff4081ca30206dd31e3cef133ed85109b
Eric Escobar <eryc@live.com.ar>
2022-09-05 16:57:06 -0300

    Prueba push bran Eryc

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
       <BrowserRouter>
 
           <NavBar />
-          <h1>Prueba push branch eryc</h1>
           <Routes>
             <Route path='/login' element={ <LoginContainer /> } />
             <Route path="*" element={<h1>404</h1>} />
