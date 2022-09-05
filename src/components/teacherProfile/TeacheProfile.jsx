35115bf799df2dd0870918af62727fe78cab3631
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-09-05 11:31:32 -0300

    creacion de componentes perfil profesor

---
+++ src/components/teacherProfile/TeacheProfile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function TeacheProfile() {
+  return (
+    <div>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <div class="name">nombre y apellido</div>
+        <div class="email">correo electronico</div>
+        <div class="phone">numero de celular</div>
+        <div class="country">pais,ciudad</div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default TeacheProfile
