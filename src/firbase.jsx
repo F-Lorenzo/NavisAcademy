8ffe6280bceb6526523edcaa044a56e9cf492321
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-09-10 19:03:50 -0300

    firbase iniciado

---
+++ src/firbase.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firstore from "firebase/firstore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRLN_2xzTrvHfN4EDRtETLysu2QWCd9Fk",
+  authDomain: "navisacademy-1c1d2.firebaseapp.com",
+  projectId: "navisacademy-1c1d2",
+  storageBucket: "navisacademy-1c1d2.appspot.com",
+  messagingSenderId: "138551732835",
+  appId: "1:138551732835:web:ce9005d6cde0708230e656",
+  measurementId: "G-VC1HXL8EM6",
+};
+
+const Fb = initializeApp(firebaseConfig);
+const db = Fb.firstore()
+const analytics = getAnalytics(FirbaseApp);
+
+export default db ;
