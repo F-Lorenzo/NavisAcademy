2254dd1b752633e2b42d86297f4008e8c4153c67
David Urdaneta <davidguitars26@gmail.com>
2022-10-02 21:00:30 -0300

    redisenando los componentes del home

--- src/components/Qualitys/Qualitys.jsx
+++ src/components/Qualitys/Qualitys.jsx
@@ -1,31 +1,78 @@
 import React from "react";
 import "./Quality.css";
+import reloj from "../../assets/img/reloj.svg";
+import notas from "../../assets/img/notas.svg";
+import objetivo from "../../assets/img/objetivo.svg";
+import arrow1 from "../../assets/img/flechaderecha.svg";
+import arrow2 from "../../assets/img/flechaizquierda.svg";
 
 const Qualitys = () => {
   return (
     <>
-      <div className="quality_Content">
+      <div className="quality__Title">
+        {" "}
         <h2>Cualidades</h2>
-
+      </div>
+      <div className="quality_Content">
         <div className="quality_Body">
+        <div className='arrow1'>
+                <img src={arrow2} alt="" />
+              </div>
           <div className="box__1">
             <div className="box__body">
-              <span>logo</span>
-              <span>texto</span>
+            
+              <div className="quality__Svg">
+                {" "}
+                <img src={reloj} alt=" " />
+              </div>
+                <div>
+                <h4>Flexible con tu tiempo</h4>
+                <span>
+                 Solo tú decides cuando estudiar.
+              </span>
+                </div>
+             
+             
             </div>
           </div>
           <div className="box__2">
             <div className="box__body">
-              <span>logo</span>
-              <span>texto</span>
+              <div className="quality__Svg">
+                {" "}
+                <img src={notas} alt=" " />
+              </div>
+              <div>
+                <h4> 100% interactivo</h4>
+                <span>
+                Clases guiadas y ejercicios que puedes hacer
+                con tu profesor y por tu cuenta.
+              </span>
+              </div>
+             
             </div>
           </div>
           <div className="box__3">
             <div className="box__body">
-              <span>logo</span>
-              <span>texto</span>
+              <div className="quality__Svg">
+                {" "}
+                <img src={objetivo} alt=" " />
+              </div>
+              <div>
+                <h4>
+                Adaptados a tus objetivos.
+                </h4>
+                <span>
+                te preparamos para entrevistas,
+                viajes, examen académicos, TOEFL y más.
+              </span>
+              </div>
+            
             </div>
+            
           </div>
+          <div className='arrow1'>
+                <img src={arrow1} alt="" />
+              </div>
         </div>
       </div>
     </>
--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,29 +1,56 @@
 import React from "react";
 import "./Questions.css";
+import arrow1 from "../../assets/img/flechaderecha.svg";
+import arrow2 from "../../assets/img/flechaizquierda.svg";
 
 const Questions = () => {
   return (
     <>
-      <div className="question_Content">
+      <div className="question__Title">
         <h2>PREGUNTAS FRECUENTES</h2>
-
+      </div>
+    
+      <div className="question_Content">
+     
         <div className="question_Body">
+        <div className='arrow1'>
+                <img src={arrow2} alt="" />
+              </div>
           <div className="question__1">
+            <h2>¿Qué es Navis?</h2>
             <div className="question__body">
-              <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime optio, unde voluptatibus quas molestiae architecto qui? Repudiandae dolores illum soluta omnis, pariatur consectetur dicta aperiam vel officiis deserunt, alias amet.</span>
+              <span>
+              Lorem ipsum dolor sit amet,
+consectetuer adipiscing elit.
+Lorem ipsum dolor sit amet,
+consectetuer adipis.
+              </span>
             </div>
           </div>
           <div className="question__2">
+            <h2>¿Cuándo puedo estudiar?</h2>
             <div className="question__body">
-              <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti labore rerum saepe commodi modi, sit assumenda quidem amet ducimus fugit aliquid doloribus dolores distinctio et minus reiciendis excepturi temporibus eligendi.</span>
+              <span>
+              Lorem ipsum dolor sit amet,
+consectetuer adipiscing elit.
+              </span>
             </div>
           </div>
           <div className="question__3">
+            <h2>¿Puedo elegir o cambiar de profesor?</h2>
             <div className="question__body">
-              <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde laborum provident veniam incidunt beatae aperiam illum, pariatur laboriosam fuga? Quis harum ullam rem animi minima aliquid, cum facere laudantium culpa.</span>
+              <span>
+              Lorem ipsum dolor sit amet,
+consectetuer adipiscing elit.
+
+              </span>
             </div>
           </div>
+          <div className='arrow1'>
+                <img src={arrow1} alt="" />
+              </div>
         </div>
+       
       </div>
     </>
   );
