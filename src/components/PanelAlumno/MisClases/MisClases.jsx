1198b55b8b78749a068846c4ee4e592f9e1388cd
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-10-20 00:12:04 -0300

    cambios en mis clases del alumno

--- src/components/PanelAlumno/MisClases/MisClases.jsx
+++ src/components/PanelAlumno/MisClases/MisClases.jsx
@@ -6,8 +6,6 @@ import { doc, getFirestore, updateDoc, increment } from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
 
 
-
-
 const MisClases = (clases) => {
 
   const { user } = UserAuth();
@@ -18,47 +16,29 @@ const MisClases = (clases) => {
       let actualValue = clases.remainingClases;
       console.log(actualValue);
       const firestore = getFirestore();
-      const userClases = doc(firestore, `Users/${user.uid}`);
+      const userClases = doc(firestore, Users/${user.uid});
       await updateDoc(userClases, {
         remainingClases: actualValue - 1,
         completedClases: increment(1),
       });
-      swal("Muy Bien", `Preparate para tu clase`, "success");
+      swal("Muy Bien", Preparate para tu clase, "success");
     } catch (e) {
-      swal("UPS!", `${e.message}`, "error");
+      swal("UPS!", ${e.message}, "error");
     }
   };
 
   const handleProgramarClases = () => {
-    swal("Muy Bien", `Ahora programaras tus clases`, "success");
+    swal("Muy Bien", Ahora programaras tus clases, "success");
     navigate("/Account/ProgramarClases");
   };
 
   const handleCalendarioDeClases = () => {
     navigate("/Account/CalendarioDeClases");
-    swal("BIENVENIDO", `Aquí podras ver tu calendario de clases`, "success");
+    swal("BIENVENIDO", Aquí podras ver tu calendario de clases, "success");
   };
-
-
   return (
     <div>
 
-          <p>Tu proxima clase esta por comenzar</p>
-                 <button
-                  className="class__button-start"
-                  // onClick={handleProgramarClases} debe redireccionar al link de la clase una vez que el profesor inicia la clase
-                >
-                  <span>iniciar clase</span>
-                </button>
-                <button
-                  className="class__button-end"
-                  onClick={handleProgramarClases}
-                  disabled={clases.remainingClases === clases.programedClases}
-                >
-                  <span>Programar clases</span>
-                </button>
-        </div>
-      </div>
       <div className="info-container">
 
         <div className="info">
@@ -68,20 +48,33 @@ const MisClases = (clases) => {
         <div className="info">
           <h3>CLASES PROGRAMADAS: {clases.programedClases} </h3>
         </div>
-        <div className="info__Content">
+
+        {/* <div className='info'>
+                    <h3>CLASES PROGRAMADAS : { clases.programedClases } </h3>
+                </div> /}
+        <div className="info-button-container">
+          {/ <div className='info-button'>
+                        <button onClick={handleInicioDeClase} disabled={clases.programedClases === 0}><h3>INICIO DE CLASE</h3></button>
+                    </div> */}
+          <div className="info__Content">
             <div className="info-button-classes-container">
+              <div className="info-button">
+                <button
+                  onClick={handleProgramarClases}
+                  disabled={clases.remainingClases === clases.programedClases}
+                >
+                  <h3>Programar clases</h3>
+                </button>
+              </div>
               <div className="info-button">
                 <button
                   onClick={handleCalendarioDeClases}
                   disabled={clases.programedClases === 0}
                 >
-                  <span>Calendario de clases</span>
+                  <h3>Calendario de clases</h3>
                 </button>
-                <button
-                  // onClick={handleCalendarioDeClases} boton que te envia a tu perfil
-                >
-                  <span>Perfil</span>
-                </button>    
+              </div>
+            </div>
           </div>
           <div className="info-button-perfil">
             <h3>Perfil</h3>
