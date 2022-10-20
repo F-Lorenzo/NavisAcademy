2b601c92cfc803b0e55587b80377d22290fbb4f1
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-10-20 11:40:40 -0300

    mis clases arreglado

--- src/components/PanelAlumno/MisClases/MisClases.jsx
+++ src/components/PanelAlumno/MisClases/MisClases.jsx
@@ -5,18 +5,13 @@ import { UserAuth } from "../../../Context/AuthContext";
 import { doc, getFirestore, updateDoc, increment } from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
 
-
-
-
 const MisClases = (clases) => {
-
   const { user } = UserAuth();
   const navigate = useNavigate();
 
   const handleInicioDeClase = async () => {
     try {
       let actualValue = clases.remainingClases;
-      console.log(actualValue);
       const firestore = getFirestore();
       const userClases = doc(firestore, `Users/${user.uid}`);
       await updateDoc(userClases, {
@@ -38,28 +33,9 @@ const MisClases = (clases) => {
     navigate("/Account/CalendarioDeClases");
     swal("BIENVENIDO", `Aquí podras ver tu calendario de clases`, "success");
   };
-
-
   return (
     <div>
-          <p>Tu proxima clase esta por comenzar</p>
-                 <button
-                  className="class__button-start"
-                  // onClick={handleProgramarClases} debe redireccionar al link de la clase una vez que el profesor inicia la clase
-                >
-                  <span>iniciar clase</span>
-                </button>
-                <button
-                  className="class__button-end"
-                  onClick={handleProgramarClases}
-                  disabled={clases.remainingClases === clases.programedClases}
-                >
-                  <span>Programar clases</span>
-                </button>
-        </div>
-      </div>
       <div className="info-container">
-
         <div className="info">
           <h3>CLASES DISPONIBLES : {clases.remainingClases}</h3>
         </div>
@@ -67,26 +43,29 @@ const MisClases = (clases) => {
         <div className="info">
           <h3>CLASES PROGRAMADAS: {clases.programedClases} </h3>
         </div>
+
         <div className="info__Content">
-            <div className="info-button-classes-container">
-              <div className="info-button">
-                <button
-                  onClick={handleCalendarioDeClases}
-                  disabled={clases.programedClases === 0}
-                >
-                  <span>Calendario de clases</span>
-                </button>
-                <button
-                  // onClick={handleCalendarioDeClases} boton que te envia a tu perfil
-                >
-                  <span>Perfil</span>
-                </button>    
+          <div className="info-button-classes-container">
+            <div className="info-button">
+              <button
+                onClick={handleProgramarClases}
+                disabled={clases.remainingClases === clases.programedClases}
+              >
+                <h3>Programar clases</h3>
+              </button>
+            </div>
+            <div className="info-button">
+              <button
+                onClick={handleCalendarioDeClases}
+                disabled={clases.programedClases === 0}
+              >
+                <h3>Calendario de clases</h3>
+              </button>
+            </div>
           </div>
-          <div className="info-button-perfil">
-            <h3>Perfil</h3>
-          </div>
-
-
+        </div>
+        <div className="info-button-perfil">
+          <h3>Perfil</h3>
         </div>
       </div>
     </div>
