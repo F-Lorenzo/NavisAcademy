e8ffb24a26bf70108030fb4a522237ce833663f3
Eric Escobar <eryc@live.com.ar>
2022-10-20 18:20:40 -0300

    Temp

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -18,10 +18,10 @@ const Admin = () => {
     return (
         <div className='admin__Options'>
             <div className='admin__Body'>
-            <h2>ADMIN ACCOUNT</h2>
+            <h2>PANEL DE ADMINISTRADOR</h2>
             <div className='admin__Button'>
-            <button className='card__Admin__Buttons'onClick={handleCreateTeacher}>CREATE A TEACHERS ACCOUNT</button>
-            <button className='card__Admin__Buttons' onClick={handleProgramingClassesAsAdmin}>PROGRAMING CLASSES</button>
+            <button className='card__Admin__Buttons'onClick={handleCreateTeacher}>CREAR CUENTA DE PROFESOR</button>
+            <button className='card__Admin__Buttons' onClick={handleProgramingClassesAsAdmin}>PROGRAMAR CLASES</button>
             </div>
          
             </div>
