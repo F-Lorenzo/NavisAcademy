7a9fff953cbd8b9448aa689dd31babb9b00fcd19
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-10-21 12:04:43 -0300

    .

--- src/components/checkout/Payment.jsx
+++ src/components/checkout/Payment.jsx
@@ -1,28 +1,28 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-import initialOptions from '../paypal/paypal.config'
-
+import initialOptions from "../paypal/paypal.config";
 
 export default function Payment() {
-    return (
-        <PayPalScriptProvider options={initialOptions}>
-            <PayPalButtons 
-              createOrder={(data, actions) => {
-                 return actions.order.create({
-                        purchase_units: [
-                          {
-                            amount: {
-                              value: "10",
-                            },
-                          },
-                        ],
-                      });
-                    }}
-              onApprove={async (data, actions) => {
-                const details = await actions.order.capture();
-                const name = details.payer.name.given_name;
-                // alert(`${quantity} clases se han agregado`);
-              }}            
-            />
-        </PayPalScriptProvider>
-    );
+  return (
+    <PayPalScriptProvider deferLoading={true} options={initialOptions}>
+      {isPending ? <div className="spinner" /> : null}
+      <PayPalButtons
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: "10",
+                },
+              },
+            ],
+          });
+        }}
+        onApprove={async (data, actions) => {
+          const details = await actions.order.capture();
+          const name = details.payer.name.given_name;
+          // alert(`${quantity} clases se han agregado`);
+        }}
+      />
+    </PayPalScriptProvider>
+  );
 }
