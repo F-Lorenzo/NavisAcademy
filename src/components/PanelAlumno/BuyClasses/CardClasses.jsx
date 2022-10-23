bf492704457f4df736541780a63cae4b7c4ffe86
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-10-23 15:50:38 -0300

    .

--- src/components/PanelAlumno/BuyClasses/CardClasses.jsx
+++ src/components/PanelAlumno/BuyClasses/CardClasses.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { UserAuth } from "../../../Context/AuthContext";
+import { getFirestore, doc, increment, updateDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import Checkout from "../../checkout/Checkout";
 import Card from "./Card";
@@ -9,21 +11,49 @@ const CardClasses = ({ number, price, duration, amount }) => {
   const precio = parseFloat(price);
   const cantidad = parseFloat(amount);
   const totalValue = cantidad * precio;
+  const { user } = UserAuth();
+  const handleBuyNow = async () => {
+    try {
+      const firestore = getFirestore();
+      const userClases = doc(firestore, `Users/${user.uid}`);
+      console.log(userClases);
+      await updateDoc(userClases, {
+        remainingClases: increment(amount),
+      });
+      setCheckout(true);
+      // swal("Muy Bien", `Adquiriste ${amount} nuevas clases`, "success");
+    } catch (e) {
+      swal("UPS!", `${e.message}`, "error");
+    }
+  };
 
-  if (checkout) {
+  if (Checkout) {
     return <Checkout totalValue={totalValue} />;
   }
 
   return (
-    <div>
-      {
-        <Card
-          number={number}
-          price={price}
-          duration={duration}
-          amount={amount}
-        />
-      }
+    // <div>
+    //   {
+    //     <Card
+    //       number={number}
+    //       price={price}
+    //       duration={duration}
+    //       amount={amount}
+    //     />
+    //   }
+    // </div>
+    <div className="buy-card">
+      <ul>
+        <li className="pack-number">Pack Nº : {number} </li>
+        <li className="amount">Cantidad de clases {amount} </li>
+        <li className="price">$ {price} USD/Clase </li>
+        <li className="duration"> Duración : {duration} min/Class </li>
+        <div>
+          <button className="button__Card" onClick={handleBuyNow}>
+            ADQUIRIR CLASES
+          </button>
+        </div>
+      </ul>
     </div>
   );
 };
--- src/components/checkout/Payment.jsx
+++ src/components/checkout/Payment.jsx
@@ -1,48 +1,132 @@
 import React, { useState } from "react";
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import initialOptions from "../paypal/paypal.config";
 
-function Payment({ total }) {
-  const [success, setSuccess] = useState(false);
-  const [ErrorMessage, setErrorMessage] = useState("");
-  const [orderID, setOrderID] = useState(false);
+import { useEffect } from "react";
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
 
-  // creates a paypal order
-  const createOrder = (data, actions) => {
-    return actions.order.create({
-      purchase_units: [
-        {
-          description: `compraste clases`,
-          amount: {
-            currency_code: "USD",
-            value: { total },
-          },
-        },
-      ],
+// This values are the props in the UI
+const amount = "2";
+const currency = "USD";
+const style = { layout: "vertical" };
+
+// Custom component to wrap the PayPalButtons and handle currency changes
+const ButtonWrapper = ({ currency, showSpinner }) => {
+  // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+  // This is the main reason to wrap the PayPalButtons in a new component
+  const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+  useEffect(() => {
+    dispatch({
+      type: "resetOptions",
+      value: {
+        ...options,
+        currency: currency,
+      },
     });
-    // .then((orderID) => {
-    //   setOrderID(orderID);
-    //   return orderID;
-    // });
-  };
+  }, [currency, showSpinner]);
 
-  // // check Approval
-  // const onApprove = async (data, actions) => {
-  //   const order = await actions.order.capture();
-  //   console.log("order", order);
-
-  //   handleApprove(orderID);
-  // };
-  // //capture likely error
-  // const onError = (data, actions) => {
-  //   setErrorMessage("Ocurrio un error con tu pago");
-  // };
-  // createOrder={createOrder} onApprove={onApprove}
   return (
-    <PayPalScriptProvider options={initialOptions}>
-      <PayPalButtons />
-    </PayPalScriptProvider>
+    <>
+      {showSpinner && isPending && <div className="spinner" />}
+      <PayPalButtons
+        style={style}
+        disabled={false}
+        forceReRender={[amount, currency, style]}
+        fundingSource={undefined}
+        createOrder={async (data, actions) => {
+          const orderId = await actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  currency_code: currency,
+                  value: amount,
+                },
+              },
+            ],
+          });
+          return orderId;
+        }}
+        onApprove={async function (data, actions) {
+          return actions.order.capture().then(function () {
+            // Your code here after capture the order
+          });
+        }}
+      />
+    </>
+  );
+};
+
+function Payment() {
+  return (
+    <div style={{ maxWidth: "750px", minHeight: "200px" }}>
+      <PayPalScriptProvider options={initialOptions}>
+        <ButtonWrapper currency={currency} showSpinner={false} />
+      </PayPalScriptProvider>
+    </div>
   );
 }
+// function Payment({ total }) {
+// const [success, setSuccess] = useState(false);
+// const [ErrorMessage, setErrorMessage] = useState("");
+// const [orderID, setOrderID] = useState(false);
+
+// // creates a paypal order
+// const createOrder = (data, actions) => {
+//   return actions.order.create({
+//     purchase_units: [
+//       {
+//         description: `compraste clases`,
+//         amount: {
+//           currency_code: "USD",
+//           value: { total },
+//         },
+//       },
+//     ],
+//   });
+//   .then((orderID) => {
+//     setOrderID(orderID);
+//     return orderID;
+//   });
+// };
+
+// // check Approval
+// const onApprove = async (data, actions) => {
+//   const order = await actions.order.capture();
+//   console.log("order", order);
+
+//   handleApprove(orderID);
+// };
+// //capture likely error
+// const onError = (data, actions) => {
+//   setErrorMessage("Ocurrio un error con tu pago");
+// };
+// createOrder={createOrder} onApprove={onApprove}
+//   return (
+//     <PayPalScriptProvider options={initialOptions}>
+//       <PayPalButtons
+//         createOrder={(data, actions) => {
+//           return actions.order.create({
+//             purchase_units: [
+//               {
+//                 amount: {
+//                   value: { total },
+//                 },
+//               },
+//             ],
+//           });
+//         }}
+//         onApprove={async (data, actions) => {
+//           const details = await actions.order.capture();
+//           const name = details.payer.name.given_name;
+//           alert(`Transaction completed by ${name}`);
+//         }}
+//       />
+//     </PayPalScriptProvider>
+//   );
+// }
 
 export default Payment;
