6e916eaa27c8734d63f92b70a02f8ab6c396d39c
Eric Escobar <eryc@live.com.ar>
2022-11-17 19:53:22 -0300

    NothingToCommit

--- src/components/Account/Notificacion.jsx
+++ src/components/Account/Notificacion.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Notificacion = ({info}) => {
-
+    
     const timeStamp = info.timeStamp.toDate().toString();
-
+    
     const handleVisto = () => {
         console.log(info);
+
     }
 
     return (
@@ -13,6 +14,7 @@ const Notificacion = ({info}) => {
             <h4>{info.textNotification}</h4>
             <p> {info.notificationType} </p>
             <p> {timeStamp} </p>
+
             <button onClick={handleVisto}>Visto</button>
         </div>
     )
