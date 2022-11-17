6c1534c666f28448e6516e4d8560592b1ddd22c1
Eric Escobar <eryc@live.com.ar>
2022-11-17 19:54:21 -0300

    NothingToCommit

--- src/components/Login/SignUp.jsx
+++ src/components/Login/SignUp.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../../Context/AuthContext';
@@ -60,81 +59,9 @@ const Signup = () => {
             setError(e.message);
             swal("UPS!", `${e.message}`, "error");
             setLoader(false);
-=======
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { UserAuth } from "../../Context/AuthContext";
-import { DateTime } from "luxon";
-import {
-  addDoc,
-  setDoc,
-  collection,
-  getFirestore,
-  doc,
-  query,
-  getDocs,
-} from "firebase/firestore";
-import Loader from "../Loader/Loader";
-
-const Signup = () => {
-  const [form, setForm] = useState({});
-  const [loader, setLoader] = useState(false);
-  const [error, setError] = useState("");
-  const { createUser } = UserAuth();
-  const navigate = useNavigate();
-  const timeStamp = DateTime.now().toFormat("DDDD - HH:mm:ss");
-
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const studentData = {
-    role: "alumn",
-    remainingClases: 0,
-    completedClases: 0,
-    absentedClases: 0,
-    teacher: "unasigned",
-    newNotifications: true,
-    notifications: 1,
-  };
-
-  const welcomeNotification = {
-    textNotification: "Bienvenido a Navis!",
-    notificationType: "Welcome",
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    setLoader(true);
-    try {
-      const infoUser = await createUser(form.email, form.password).then(
-        (firebaseData) => {
-          return firebaseData;
->>>>>>> David
         }
-      );
-      console.log(infoUser); // BORRAR ESTA SHIT!!
-      const firestore = getFirestore();
-      const docuRef = doc(firestore, `Users/${infoUser.user.uid}`);
-      setDoc(docuRef, { ...form, ...studentData });
-
-      const notifications = collection(
-        firestore,
-        `Users/${infoUser.user.uid}/myNotifications`
-      );
-      addDoc(notifications, { ...welcomeNotification, timeStamp });
-
-      navigate("/account");
-    } catch (e) {
-      setError(e.message);
-      swal("UPS!", `${e.message}`, "error");
-      setLoader(false);
-    }
-  };
+    };
+  
 
   if (loader) {
     return <Loader />;
