a6e6c889479612cee04d0143ac42795547d33076
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-12-20 16:41:52 -0300

    nuevas tarjetas

--- src/components/PanelAlumno/BuyClasses/BuyClasses.jsx
+++ src/components/PanelAlumno/BuyClasses/BuyClasses.jsx
@@ -11,79 +11,90 @@ import {
   query,
   collection,
   setDoc,
-  addDoc, serverTimestamp,
+  addDoc,
+  serverTimestamp,
 } from "firebase/firestore";
 import { Navigate, useNavigate } from "react-router-dom";
 import Checkout from "../../checkout/Checkout";
 import "./cardClasses.css";
 
+const BuyClasses = ({ duration }) => {
+  const [isCheckout, setIsCheckout] = useState(false);
 
-const BuyClasses = ({duration}) => {
+  const [totalValue, setTotalValue] = useState();
+  const [cantidad, setCantidad] = useState();
 
-    const [ isCheckout, setIsCheckout ] = useState(false);
+  let buyCards = [];
 
-    const [ totalValue, setTotalValue ] = useState();
-    const [ cantidad, setCantidad ] = useState();
+  switch (duration) {
+    case 30:
+      buyCards = CardsItems30;
+      break;
+    case 50:
+      buyCards = CardsItems50;
+      break;
+  }
 
-    let buyCards = [];
+  if (isCheckout) {
+    return <Checkout totalValue={totalValue} cantidad={cantidad} />;
+  }
 
-    switch (duration) {
-        case 30:
-            buyCards = CardsItems30;
-            break;
-        case 50:
-            buyCards = CardsItems50;
-            break;
-    }
+  return (
+    <div className="buy-container">
+      <h3>ADQUIRIR MAS CLASES</h3>
 
-    if (isCheckout) {
-        return <Checkout totalValue={totalValue} cantidad={cantidad} />
-    }
+      <div className="buy-card-container">
+        {buyCards.map((item, index) => {
+          return (
+            <div key={index} className="buy-card">
+              <li>
+                <ul>
+                  <li className="pack-number">Navis {item.number} </li>
+                  <li className="amount">
+                    {" "}
+                    {item.amount} <span className="clases-thin">Clases</span>{" "}
+                  </li>
+                  <li className="duration"> {item.duration} min/Class </li>
+                  <li className="libros">Libros interactivos </li>
+                  <li className="price">$ {item.price} USD por clase </li>
+                  <div>
+                    <div>
+                      <select>
+                        <option value="dolar" id="dolar">
+                          $
+                        </option>
+                        <option value="euro" id="euro">
+                          €
+                        </option>
+                      </select>
+                    </div>
 
-    return (
-        <div className="buy-container">
-            <h3>ADQUIRIR MAS CLASES</h3>
+                    <button
+                      className="button__Card"
+                      onClick={(handleBuyClasses) => {
+                        console.log(item);
+                        let precio = parseFloat(item.price);
+                        let amount = parseInt(item.amount);
+                        let total = amount * precio;
+                        let finalValue = total.toFixed(2).toString();
+                        setCantidad(amount);
+                        setTotalValue(finalValue);
 
-            <div className="buy-card-container">
-                { buyCards.map((item, index) => {
-                    return (
-                        <div key={index} className="buy-card">
-                            <li>
-                            <ul>
-                                <li className="pack-number">Pack Nº : {item.number} </li>
-                                <li className="amount">Cantidad de clases {item.amount} </li>
-                                <li className="price">$ {item.price} USD/Clase </li>
-                                <li className="duration"> Duración : {item.duration} min/Class </li>
-                                <div>
-                                    <div>
-                                        <select >
-                                            <option  value="dolar" id='dolar'>$</option>
-                                            <option  value="euro" id='euro'>€</option>
-                                        </select>
-                                    </div>
-                
-                                    <button className="button__Card" onClick={handleBuyClasses => {
-                                        console.log(item);
-                                        let precio = parseFloat(item.price);
-                                        let amount = parseInt(item.amount); 
-                                        let total = amount * precio;
-                                        let finalValue = total.toFixed(2).toString();
-                                        setCantidad(amount);
-                                        setTotalValue(finalValue);
-
-                                        setIsCheckout(!isCheckout);
-                                        }}>
-                                        ADQUIRIR CLASES
-                                    </button>
-                                </div>
-                            </ul>
-                            </li>
-                        </div>
-                    )
-                })}
+                        setIsCheckout(!isCheckout);
+                      }}
+                    >
+                      Comprar
+                    </button>
+                  </div>
+                </ul>
+              </li>
+              <div className="cohete"></div>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default BuyClasses;
