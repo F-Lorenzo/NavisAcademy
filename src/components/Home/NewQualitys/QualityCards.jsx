ed672707c81a795d2fe3a7e9e66fb04e83353832
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-12-26 16:53:10 -0300

    cambvios en qualitys

--- src/components/Home/NewQualitys/QualityCards.jsx
+++ src/components/Home/NewQualitys/QualityCards.jsx
@@ -3,39 +3,39 @@ import notas from "../../../assets/img/notas.svg";
 import objetivo from "../../../assets/img/objetivo.svg";
 
 export const QualityCards = [
-    {
-        title: 'Divide el inglés en 4 áreas:',
-        text: 'hablar, escuchar, vocabulario y gramática.',
-        icon: notas
-    },
-    {
-        title: 'Adaptados a tus objetivos',
-        text: 'Te preparamos para entrevistas, viajes, exámenes académicos, TOEFL y más.',
-        icon: objetivo
-    },
-    {
-        title: 'Flexible con tu tiempo',
-        text: 'Tú decides cuando estudiar.',
-        icon: reloj
-    },
-    {
-        title: 'No es una clase normal',
-        text: 'No es estática: juegos, roles, ejercicios y conversación.',
-        icon: notas
-    },
-    {
-        title: '100% Interactivo',
-        text: 'Clases guiadas, con ejercicios que puedes realizar con tu profesor o por tu cuenta.',
-        icon: notas
-    },
-    {
-        title: 'Recursos profesionales',
-        text: 'Nos basamos en el método de Cambridge, la escala de inglés global.',
-        icon: notas
-    },
-    {
-        title: 'Profesores altamente cualificados',
-        text: 'Contamos con profesores especializados en la enseñanza del inglés a niños y adultos de distintas edades.',
-        icon: notas
-    },
+  {
+    title: "Divide el inglés en 4 áreas:",
+    text: "hablar, escuchar, vocabulario y gramática.",
+    icon: notas,
+  },
+  {
+    title: "Adaptados a tus objetivos",
+    text: "Te preparamos para entrevistas, viajes, exámenes académicos, TOEFL y más.",
+    icon: objetivo,
+  },
+  {
+    title: "Flexible con tu tiempo",
+    text: "Tú decides cuando estudiar.",
+    icon: reloj,
+  },
+  {
+    title: "No es una clase normal",
+    text: "No es estática: juegos, roles, ejercicios y conversación.",
+    icon: notas,
+  },
+  {
+    title: "100% Interactivo",
+    text: "Clases guiadas, con ejercicios que puedes realizar con tu profesor o por tu cuenta.",
+    icon: notas,
+  },
+  {
+    title: "Recursos profesionales",
+    text: "Nos basamos en el método de Cambridge, la escala de inglés global.",
+    icon: notas,
+  },
+  {
+    title: "Profesores altamente cualificados",
+    text: "profesores especializados en la enseñanza del inglés a niños y adultos.",
+    icon: notas,
+  },
 ];
