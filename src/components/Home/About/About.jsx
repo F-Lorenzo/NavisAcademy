0551cdac3fe0525228e7e3a54590d7fbdeb3686f
Eric Escobar <eryc@live.com.ar>
2023-04-02 22:19:27 -0300

    fkYouTube

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "./About.scss";
+/*
 import VideoNavis from "./VideoNavis/VideoNavis";
 import VideoNavisPlay from "./VideoNavis/VideoNavisPlay";
+*/
 
 const About = () => {
 
-  const [ play, setPlay ] = useState(false);
-
-  setTimeout(() => {
-      setPlay(true);
-  }, 10000);
+  /*
+  
+    const [ play, setPlay ] = useState(false);
+  
+    setTimeout(() => {
+        setPlay(true);
+    }, 10000);
+  */
 
   return (
     <section className="about_container">
@@ -28,7 +33,11 @@ const About = () => {
         </span>
       </div>
 
-      { play === true ?
+      
+
+      {
+      /*
+        play === true ?
         <div className="about">
           <VideoNavisPlay /> 
         </div>
@@ -36,6 +45,7 @@ const About = () => {
         <div className="about">
           <VideoNavis /> 
         </div>
+      */
       }
       
     </section>
